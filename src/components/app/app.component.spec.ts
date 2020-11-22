2be548cfbc21b2f9846668295b835143eb69c3f2
Raman Shyrski <Raman_Shyrski@epam.com>
2020-11-22 14:11:16 +0300

    Add home page

--- src/components/app/app.component.spec.ts
+++ src/components/app/app.component.spec.ts
@@ -30,6 +30,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('greatest-me app is running!');
+    expect(compiled.querySelector('.page-wrapper span').textContent).toContain('greatest-me app is running!');
   });
 });
